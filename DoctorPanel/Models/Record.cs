496c97b7ac71a12c52682fe35161f86d54e348b6
Rtr04G <rtg.r04@mail.ru>
2023-12-26 22:52:04 +0300

    Добавил таблицу пациентов. Удалил неиспользуемую модель.

---
+++ DoctorPanel/Models/Record.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DoctorPanel.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string DoctorId{ get; set; }
+        public string PatientId { get; set; }
+        public DateTime DateX { get; set;}
+    }
+}
